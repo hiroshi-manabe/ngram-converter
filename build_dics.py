0c54073caa78617d5bd898d6bfbf1cc879fca2df
Hiroshi Manabe <t_takeda25@hotmail.com>
2011-07-26 00:31:16 +0900

    Changed the Shabang line.

--- build_dics.py
+++ build_dics.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import ngram_converter.converter
 
 import getopt
--- converter_sample.py
+++ converter_sample.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import ngram_converter.converter
